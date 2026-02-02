d1df9ec6a543e623136fb1adcbd458d2e76b282e
Andrew Siwko <asiwko@siwko.org>
2026-02-02 14:09:39 -0500

    add ICMP

--- 03-security-group.tf
+++ 03-security-group.tf
@@ -29,3 +29,14 @@ resource "ibm_is_security_group_rule" "outbound_rhel_activation" {
   port_min  = 80
   port_max  = 443 
 }
+resource "ibm_is_security_group_rule" "inbound_icmp" {
+  group     = ibm_is_security_group.asiwko_sg.id
+  direction = "inbound"
+  remote    = "0.0.0.0/0"
+  protocol  = "icmp"
+  
+  protocol = "icmp"
+  type = 8
+  code = 0
+  }
+}
