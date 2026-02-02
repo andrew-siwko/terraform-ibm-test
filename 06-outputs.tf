a6b79b0b9d401dad8e80480f9580ebc7ef84275d
Andrew Siwko <asiwko@siwko.org>
2026-02-02 12:32:33 -0500

    remove google outputs

--- 06-outputs.tf
+++ 06-outputs.tf
@@ -1,11 +1,3 @@
-output "network_info" {
-  value = google_compute_network.vpc_network.*
-}
-
-output "public_ip" {
-  value = google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip
-}
-
 output  "asiwko-vpc" {
   value= ibm_is_vpc.asiwko-vpc.*
 }
