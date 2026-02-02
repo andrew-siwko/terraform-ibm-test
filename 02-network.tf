3648ab9b560a726737920568046a919dba7b7885
Andrew Siwko <asiwko@siwko.org>
2026-02-02 12:53:26 -0500

    cleanup ; added ip

--- 02-network.tf
+++ 02-network.tf
@@ -1,10 +1,15 @@
-resource "ibm_is_vpc" "asiwko-vpc" {
+resource "ibm_is_vpc" "asiwko_vpc" {
   name = "asiwko-vpc"
 }
 
-resource "ibm_is_subnet" "asiwko-subnet" {
+resource "ibm_is_subnet" "asiwko_subnet" {
   name            = "asiwko-subnet"
-  vpc             = ibm_is_vpc.asiwko-vpc.id
+  vpc             = ibm_is_vpc.asiwko_vpc.id
   zone            = "us-south-1"
   ipv4_cidr_block = "10.240.0.0/24"
+}
+
+resource "ibm_is_floating_ip" "asiwko_public_ip" {
+  name   = "asiwko-vm-01-fip"
+  target = ibm_is_instance.asiwko-vm-01.primary_network_interface[0].id
 }
