5de7e90763a1ec9b5d104e7daeb0038a61160a14
Andrew Siwko <asiwko@siwko.org>
2026-02-02 12:40:32 -0500

    reformatted ; added public ip

--- 06-outputs.tf
+++ 06-outputs.tf
@@ -1,11 +1,15 @@
-output  "asiwko-vpc" {
-  value= ibm_is_vpc.asiwko-vpc.*
+output "asiwko-vpc" {
+  value = ibm_is_vpc.asiwko-vpc.*
 }
 
-output "asiwko-subnet"{
-  value= ibm_is_subnet.asiwko-subnet.*
+output "asiwko-subnet" {
+  value = ibm_is_subnet.asiwko-subnet.*
 }
 
 output "asiwko-vm-01" {
-  value=ibm_is_instance.asiwko-vm-01.*
+  value = ibm_is_instance.asiwko-vm-01.*
+}
+
+output "asiwko-public-ip" {
+  value = ibm_is_floating_ip.asiwko_public_ip
 }
