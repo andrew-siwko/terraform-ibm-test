6c07c456c2cc48dbc82abd1fa9ccce40c856b080
Andrew Siwko <asiwko@siwko.org>
2026-02-02 15:35:53 -0500

    add public gateways

--- 02-network.tf
+++ 02-network.tf
@@ -14,3 +14,24 @@ resource "ibm_is_subnet" "asiwko_subnet_2" {
   zone            = "us-south-2"
   ipv4_cidr_block = "10.240.64.0/24"
 }
+
+resource "ibm_is_public_gateway" "asiwko_pgw_z1" {
+  name = "asiwko-pgw-z1"
+  vpc  = ibm_is_vpc.asiwko_vpc.id
+  zone = "us-south-1"
+}
+
+resource "ibm_is_public_gateway" "asiwko_pgw_z2" {
+  name = "asiwko-pgw-z2"
+  vpc  = ibm_is_vpc.asiwko_vpc.id
+  zone = "us-south-2"
+}
+
+resource "ibm_is_subnet_public_gateway_attachment" "asiwko_pgw_attach_z1" {
+  subnet         = ibm_is_subnet.asiwko_subnet_1.id
+  public_gateway = ibm_is_public_gateway.asiwko_pgw_z1.id
+}
+resource "ibm_is_subnet_public_gateway_attachment" "asiwko_pgw_attach_z2" {
+  subnet         = ibm_is_subnet.asiwko_subnet_2.id
+  public_gateway = ibm_is_public_gateway.asiwko_pgw_z2.id
+}
