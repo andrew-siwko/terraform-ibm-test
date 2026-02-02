5d235d1711957b256e955ecac17bba6e1ae5ce9a
Andrew Siwko <asiwko@siwko.org>
2026-02-02 13:50:26 -0500

    updated subnet; revert profile

--- 04-machines.tf
+++ 04-machines.tf
@@ -10,16 +10,16 @@ data "ibm_is_image" "rhel9_image" {
 resource "ibm_is_instance" "asiwko-vm-01" {
   name    = "asiwko-vm-01"
   image   = data.ibm_is_image.rhel9_image.id
-  # profile = "cx2-2x4"
-  profile = "bxf-2x8"
+  profile = "cx2-2x4"
+  # profile = "bxf-2x8"
 
   primary_network_interface {
-    subnet          = ibm_is_subnet.asiwko_subnet_1.id
+    subnet          = ibm_is_subnet.asiwko_subnet_2.id
     security_groups = [ibm_is_security_group.asiwko_sg.id]
   }
 
   vpc  = ibm_is_vpc.asiwko_vpc.id
-  zone = "us-south-1"
+  zone = "us-south-2"
   keys = [ibm_is_ssh_key.ssh_key.id]
 }
 
