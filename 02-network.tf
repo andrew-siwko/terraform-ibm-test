9521dbe2a3ef01e8393ec10f48a3d8f7419020ec
Andrew Siwko <asiwko@siwko.org>
2026-02-02 12:32:21 -0500

    update vpc names

--- 02-network.tf
+++ 02-network.tf
@@ -1,9 +1,9 @@
 resource "ibm_is_vpc" "asiwko-vpc" {
-  name = "terraform-vpc"
+  name = "asiwko-vpc"
 }
 
 resource "ibm_is_subnet" "asiwko-subnet" {
-  name            = "terraform-subnet"
+  name            = "asiwko-subnet"
   vpc             = ibm_is_vpc.asiwko-vpc.id
   zone            = "us-south-1"
   ipv4_cidr_block = "10.240.0.0/24"
