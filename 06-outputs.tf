54d8c68889c60bf6cb5bad1f98d1095a32f1c95b
Andrew Siwko <asiwko@siwko.org>
2026-02-02 16:07:10 -0500

    added an ssh command for the instance

--- 06-outputs.tf
+++ 06-outputs.tf
@@ -16,3 +16,9 @@ output "asiwko-vm-01" {
 output "asiwko-public-ip" {
   value = ibm_is_floating_ip.asiwko_public_ip.address
 }
+
+# recommended by gemini: 
+output "FINAL_SSH_COMMAND" {
+  description = "Copy and paste this into your terminal to login"
+  value       = "ssh -i /container_shared/ansible/ansible_rsa root@${ibm_is_floating_ip.asiwko_public_ip.address}"
+}
