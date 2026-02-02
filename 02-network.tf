e8f5c68eff95576e451be13a0e42ae17d26cd66c
Andrew Siwko <asiwko@siwko.org>
2026-02-02 14:46:49 -0500

    fixed subnet names

--- 02-network.tf
+++ 02-network.tf
@@ -3,13 +3,13 @@ resource "ibm_is_vpc" "asiwko_vpc" {
 }
 
 resource "ibm_is_subnet" "asiwko_subnet_1" {
-  name            = "asiwko-subnet"
+  name            = "asiwko-subnet-1"
   vpc             = ibm_is_vpc.asiwko_vpc.id
   zone            = "us-south-1"
   ipv4_cidr_block = "10.240.0.0/24"
 }
 resource "ibm_is_subnet" "asiwko_subnet_2" {
-  name            = "asiwko-subnet-z2"
+  name            = "asiwko-subnet-2"
   vpc             = ibm_is_vpc.asiwko_vpc.id
   zone            = "us-south-2"
   ipv4_cidr_block = "10.240.64.0/24"
