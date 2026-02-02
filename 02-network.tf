c3afc495b3a4976500e228d3799b37d0b3cde78c
Andrew Siwko <asiwko@siwko.org>
2026-02-02 15:46:13 -0500

    let's add south-3 for later

--- 02-network.tf
+++ 02-network.tf
@@ -15,6 +15,14 @@ resource "ibm_is_subnet" "asiwko_subnet_2" {
   ipv4_cidr_block = "10.240.64.0/24"
 }
 
+resource "ibm_is_subnet" "asiwko_subnet_3" {
+  name            = "asiwko-subnet-3"
+  vpc             = ibm_is_vpc.asiwko_vpc.id
+  zone            = "us-south-3"
+  ipv4_cidr_block = "10.240.128.0/24"
+}
+
+
 resource "ibm_is_public_gateway" "asiwko_pgw_z1" {
   name = "asiwko-pgw-z1"
   vpc  = ibm_is_vpc.asiwko_vpc.id
@@ -27,6 +35,12 @@ resource "ibm_is_public_gateway" "asiwko_pgw_z2" {
   zone = "us-south-2"
 }
 
+resource "ibm_is_public_gateway" "asiwko_pgw_z3" {
+  name = "asiwko-pgw-z3"
+  vpc  = ibm_is_vpc.asiwko_vpc.id
+  zone = "us-south-3"
+}
+
 resource "ibm_is_subnet_public_gateway_attachment" "asiwko_pgw_attach_z1" {
   subnet         = ibm_is_subnet.asiwko_subnet_1.id
   public_gateway = ibm_is_public_gateway.asiwko_pgw_z1.id
@@ -35,3 +49,11 @@ resource "ibm_is_subnet_public_gateway_attachment" "asiwko_pgw_attach_z2" {
   subnet         = ibm_is_subnet.asiwko_subnet_2.id
   public_gateway = ibm_is_public_gateway.asiwko_pgw_z2.id
 }
+resource "ibm_is_subnet_public_gateway_attachment" "asiwko_pgw_attach_z3" {
+  subnet         = ibm_is_subnet.asiwko_subnet_3.id
+  public_gateway = ibm_is_public_gateway.asiwko_pgw_z3.id
+}
+# resource "ibm_is_route_table" "asiwko_route_table" {
+#   name = "asiwko-route-table"
+#   vpc  = ibm_is_vpc.asiwko_vpc.id
+# }
