9e292d0aeaa711516f467950aa6fb177907d2a4e
Andrew Siwko <asiwko@siwko.org>
2026-02-04 16:44:47 -0500

    remove unneeded comment

--- 01-provider.tf
+++ 01-provider.tf
@@ -23,7 +23,6 @@ terraform {
     skip_requesting_account_id = true
     skip_s3_checksum           = true
 
-    # These should stay as previously configured:
     use_path_style              = true
     skip_region_validation      = true
     skip_credentials_validation = true
